b5021116259a8bc6f87053a0a0cff1af7c1267b4
hexinbei <hexinbei1224@gmail.com>
2021-07-16 15:20:04 +0800

    [A]拆成stateless

--- lib/main.dart
+++ lib/main.dart
@@ -20,109 +20,7 @@ class MyApp extends StatelessWidget {
 class Calculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Calculator'),
-        ),
-        body: Container(
-            color: Colors.black,
-            child: Container(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                        child: Container(
-                            alignment: FractionalOffset.bottomRight,
-                            color: Colors.black,
-                            height: 200,
-                            child: Text(
-                              '0',
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 70),
-                            ))),
-                    Container(
-                        color: Colors.black38,
-                        height: 480,
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  const Keyboard("AC", 0),
-                                  const Keyboard("7", 1),
-                                  const Keyboard("4", 1),
-                                  const Keyboard("1", 1),
-                                  Expanded(
-                                    child: Container(
-                                        color: Colors.grey,
-                                        padding: EdgeInsets.only(left: 90),
-                                        margin: EdgeInsets.only(
-                                            right: 0,
-                                            top: 1,
-                                            left: 1,
-                                            bottom: 1),
-                                        child: Align(
-                                          child: Text(
-                                            '0',
-                                            textAlign: TextAlign.right,
-                                            style: TextStyle(
-                                                fontSize: 28,
-                                                color: Colors.white),
-                                          ),
-                                        )),
-                                    flex: 1,
-                                  ),
-                                ],
-                              ),
-                              flex: 1,
-                            ),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  const Keyboard("+/-", 0),
-                                  const Keyboard("8", 1),
-                                  const Keyboard('5', 1),
-                                  const Keyboard('2', 1),
-                                  Expanded(
-                                    child: Container(
-                                      color: Colors.grey,
-                                      margin: EdgeInsets.only(
-                                          left: 0, top: 1, right: 1, bottom: 1),
-                                    ),
-                                    flex: 1,
-                                  ),
-                                ],
-                              ),
-                              flex: 1,
-                            ),
-                            Expanded(
-                              child: Column(
-                                children: [
-                                  const Keyboard('%', 0),
-                                  const Keyboard('9', 1),
-                                  const Keyboard('6', 1),
-                                  const Keyboard('3', 1),
-                                  const Keyboard('.', 1),
-                                ],
-                              ),
-                              flex: 1,
-                            ),
-                            Expanded(
-                              child: Column(
-                                children: <Widget>[
-                                  const Keyboard('/', 3),
-                                  const Keyboard('X', 3),
-                                  const Keyboard('ㄧ', 3),
-                                  const Keyboard('+', 3),
-                                  const Keyboard('=', 3),
-                                ],
-                              ),
-                              flex: 1,
-                            ),
-                          ],
-                        )),
-                  ]),
-            )));
+    return MyHomePage();
   }
 }
 
@@ -147,6 +45,40 @@ class Keyboard extends StatelessWidget {
             ),
           )),
       flex: 1,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePage();
+  }
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  int _count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator'),
+      ),
+      body: Center(
+        child: Text(
+          '$_count',
+          style: TextStyle(fontSize: 28),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ++_count;
+          });
+          print('count: $_count');
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
